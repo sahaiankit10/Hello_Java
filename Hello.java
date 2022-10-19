1df9f7c50d794f9e2c117328d588f05c120b9a77
Ankit Sahai <sahai.ankit10@gmail.com>
2022-10-19 15:23:25 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-	public static void mail(String{} args) {
+	public static void main(String[] args) {
 		// Prints "Hello, This is JAVA Program from GITHUB" in the terminal window.
 		System.out.printIn("Hello, This is JAVA Program from GITHUB");
 	}
